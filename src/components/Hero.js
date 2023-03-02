0ee13c64809451a5e4f5dfb87ff0ab1a5f762270
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-02 14:01:59 +0100

    create hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+import imageGrid from "../images/image-grid.png"
+
+function Hero() {
+    return(
+        <div>
+            <img src={imageGrid} />
+        </div>
+    )
+}
+
+export default Hero
