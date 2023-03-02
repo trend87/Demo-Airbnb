b68a94e6411b1215f32805a806e407123816bcad
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-02 14:25:08 +0100

    add contents to hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,12 @@ import imageGrid from "../images/image-grid.png"
 
 function Hero() {
     return(
-        <div>
-            <img src={imageGrid} />
-        </div>
+        <section className="hero-container">
+            <img src={imageGrid} className="image-grid" />
+            <h2>Online Experiences</h2>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+        </section>
     )
 }
 
