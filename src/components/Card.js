7790e287fa6404f618039534a64d860ad09f669c
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-03 10:40:40 +0100

    add contents to card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,22 @@
 import React from "react"
+import katie from "../images/katie.png"
+import star from "../images/star.png"
 
 function Card() {
 
-    render(
+    return(
 
+        <div className="card-container">
+            <div className="card-box">
+                <img src={katie} />
+                <span>
+                    <img src={star} /> 5.0 (6) . USA
+                </span>
+                <p>Life lessons with Katie Zaferes</p>
+                <p> <b>From $136 / </b>  person</p>
+            </div>
+            
+        </div>
     )
 }
 
