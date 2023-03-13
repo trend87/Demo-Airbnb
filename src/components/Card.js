b2b488b1c95f8355aaebc734e7597dc7447ae5ee
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-13 17:07:19 +0100

    give classname to jsx elements

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,18 @@
 import React from "react"
 
+
 function Card(props) {
 
     return(
 
         <div className="card-container">
             <div className="card-box">
-                <img src={props.img} />
-                <span>
-                    <img src={props.star} /> {props.rating } {props.reviewCount} . {props.country}
+                <img src={`../images/${props.coverImg}`} className="card-img"/>
+                <span className="card-stats">
+                <img src={`../images/${props.star}`} className="card-star" /> {props.stats } ({props.review}) • {props.location}
                 </span>
-                <p>{props.title}</p>
-                <p> <b>From {props.price} / </b>  person</p>
+                <p className="card-title">{props.title}</p>
+                <p className="card-price"> <b>From {props.price} / </b>  person</p>
             </div>
             
         </div>
