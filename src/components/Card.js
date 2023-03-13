7e68c6e6594e167cb03a1bb699ed7903a13f8f26
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-13 20:32:33 +0100

    bug, images not displaying on gh-pages

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,9 +14,9 @@ function Card(props) {
         <div className="card-container">
             
               {badgeText  && <div className="card-badge"> {badgeText}</div>}
-                <img src={`../images/${props.item.coverImg}`} className="card-img"/>
+                <img src={`./images/${props.item.coverImg}`} className="card-img"/>
                 <span className="card-stats">
-                <img src={`../images/${props.item.stats.star}`} className="card-star" /> {props.item.stats.rating } ({props.item.stats.reviewCount}) • {props.item.location}
+                <img src={`./images/${props.item.stats.star}`} className="card-star" /> {props.item.stats.rating } ({props.item.stats.reviewCount}) • {props.item.location}
                 </span>
                 <p className="card-title">{props.item.title}</p>
                 <p className="card-price"> <b>From {props.item.price} / </b>  person</p>
