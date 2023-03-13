6c7e016cd7a197251d89939b3dd252bacb9f662b
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-13 18:24:56 +0100

    conditional rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,15 @@ function Card(props) {
     return(
 
         <div className="card-container">
-            <div className="card-box">
+            
+              {props.openSpots === 0 && <div className="card-badge"> SOLD OUT</div>}
                 <img src={`../images/${props.coverImg}`} className="card-img"/>
                 <span className="card-stats">
                 <img src={`../images/${props.star}`} className="card-star" /> {props.stats } ({props.review}) • {props.location}
                 </span>
                 <p className="card-title">{props.title}</p>
                 <p className="card-price"> <b>From {props.price} / </b>  person</p>
-            </div>
+         
             
         </div>
     )
